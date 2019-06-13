750f9e1071a19f4af60984ec929a180c228e4cd8
Nathan Cairns <nathancairns777@gmail.com>
2019-06-13 17:07:55 +1200

    Spell check

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='cbt',
-      description='Generate code using machine learning',
+      description='Generating code using machine learning',
       author='Buster & Nathan',
       packages= ['cbt'],
       install_requires=[
