cce2959ac06e63f3be8f941f27fdf8c5570fc77c
Nathan Cairns <nathancairns777@gmail.com>
2019-06-13 17:06:25 +1200

    Initial setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+from setuptools import setup
+
+setup(name='cbt',
+      description='Generate code using machine learning',
+      author='Buster & Nathan',
+      packages= ['cbt'],
+      install_requires=[
+            'staticfg'
+      ])
