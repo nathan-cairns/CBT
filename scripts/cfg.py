7e4d03cc448b067e6ca78aa13094f2dafbe43e2b
Nathan Cairns <nathancairns777@gmail.com>
2019-06-13 18:13:40 +1200

    Add basic script

--- scripts/cfg.py
+++ scripts/cfg.py
@@ -1,5 +1,44 @@
 #!/usr/bin/env python3
 
 
+# This script is used for generating cfgs from python code
+# Author: Nathan Cairns
+
+
+# IMPORTS #
+
+
+from staticfg import CFGBuilder
+import os
+
+
+# CONSTANTS #
+
+
+DATA_PATH = './../data/py150_files'
+TRAINING_SET_FILE_PATH = '{}/python100k_train.txt'.format(DATA_PATH)
+EVALUATION_SET_FILE_PATH = '{}/python50k_eval.txt'.format(DATA_PATH)
+
+
+# FUNCTIONS #
+
+
+def process_set(file_path):
+    content = []
+    with open(file_path) as f:
+        content = f.readlines()
+        content = [line.strip() for line in content if os.path.basename(line.strip()) != '__init__.py']
+
+    for f in content[:3]:
+        build_cfg(f)
+
+def build_cfg(file_path):
+    cfg = CFGBuilder().build_from_file(os.path.basename(file_path), '{}/{}'.format(DATA_PATH, file_path))
+    cfg.build_visual('./{}'.format(os.path.basename(file_path)), format='pdf', show=False)
+
+
+# MAIN #
+
+
 if __name__ == '__main__':
-    pass
+    process_set(TRAINING_SET_FILE_PATH)
