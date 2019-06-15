d53ff05e875a981056279027d0f893df6e81ad1e
buster-darragh-major <busterjdarraghmajor@gmail.com>
2019-06-15 18:00:50 +1200

    Add multi os support for file paths

--- scripts/cfg.py
+++ scripts/cfg.py
@@ -15,9 +15,11 @@ import os
 # CONSTANTS #
 
 
-DATA_PATH = './../data/py150_files'
-TRAINING_SET_FILE_PATH = '{}/python100k_train.txt'.format(DATA_PATH)
-EVALUATION_SET_FILE_PATH = '{}/python50k_eval.txt'.format(DATA_PATH)
+REPO_ROOT_PATH = os.path.join(os.path.dirname(os.path.realpath(__file__)),'..')
+ERROR_LOG_FILE = os.path.join(REPO_ROOT_PATH, 'log', 'data_processing')
+DATA_PATH = os.path.join(os.path.dirname(REPO_ROOT_PATH, 'data', 'py150_files')
+TRAINING_SET_FILE_PATH = os.path.join(DATA_PATH, 'python100k_train.txt')
+EVALUATION_SET_FILE_PATH = os.path.join(DATA_PATH, 'python50k_eval.txt')
 
 
 # FUNCTIONS #
@@ -26,15 +28,21 @@ EVALUATION_SET_FILE_PATH = '{}/python50k_eval.txt'.format(DATA_PATH)
 def process_set(file_path):
     content = []
     with open(file_path) as f:
-        content = f.readlines()
         content = [line.strip() for line in content if os.path.basename(line.strip()) != '__init__.py']
 
     for f in content[5:8]:
         build_cfg(f)
 
+
 def build_cfg(file_path):
     cfg = CFGBuilder().build_from_file(os.path.basename(file_path), '{}/{}'.format(DATA_PATH, file_path))
     cfg.build_visual('./{}'.format(os.path.basename(file_path)), format='pdf')
+
+
+def handle_exception(file_path):
+    with open(ERROR_LOG_FILE, 'w+') as f:
+        print("hi")
+
 
 
 # MAIN #
