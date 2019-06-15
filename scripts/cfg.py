4aa27e8b9c562e8956b6a7f98cdf613ee4ba0b2e
buster-darragh-major <busterjdarraghmajor@gmail.com>
2019-06-15 18:33:44 +1200

    Add utf8 support for file reads and add file reading error logging

--- scripts/cfg.py
+++ scripts/cfg.py
@@ -10,28 +10,39 @@
 
 from staticfg import CFGBuilder
 import os
+import datetime
 
 
 # CONSTANTS #
 
 
-REPO_ROOT_PATH = os.path.join(os.path.dirname(os.path.realpath(__file__)),'..')
-ERROR_LOG_FILE = os.path.join(REPO_ROOT_PATH, 'log', 'data_processing')
-DATA_PATH = os.path.join(os.path.dirname(REPO_ROOT_PATH, 'data', 'py150_files')
+REPO_ROOT_PATH = os.path.join(os.path.dirname(os.path.realpath(__file__)), '..')
+DATA_PATH = os.path.join(REPO_ROOT_PATH, 'data', 'py150_files')
+
+ERROR_LOG_PATH = os.path.join(REPO_ROOT_PATH, 'log', 'dataprocessing')
+ERROR_LOG_FILE = os.path.join(ERROR_LOG_PATH, 'errorlog.csv')
+
 TRAINING_SET_FILE_PATH = os.path.join(DATA_PATH, 'python100k_train.txt')
 EVALUATION_SET_FILE_PATH = os.path.join(DATA_PATH, 'python50k_eval.txt')
 
 
 # FUNCTIONS #
 
+# TODO: if there are errors tell the user at the end
+# TODO: progress bar
 
 def process_set(file_path):
     content = []
-    with open(file_path) as f:
-        content = [line.strip() for line in content if os.path.basename(line.strip()) != '__init__.py']
+    try:
+        with open(file_path, encoding="utf8") as f:
+            content = f.readlines()
+            content = [line.strip() for line in content if os.path.basename(line.strip()) != '__init__.py']
+    except Exception as e:
+        print("exception!")
+        handle_exception(file_path, 'Error in reading file', str(e))
 
-    for f in content[5:8]:
-        build_cfg(f)
+    # for f in content[5:8]:
+    #   build_cfg(f)
 
 
 def build_cfg(file_path):
@@ -39,14 +50,14 @@ def build_cfg(file_path):
     cfg.build_visual('./{}'.format(os.path.basename(file_path)), format='pdf')
 
 
-def handle_exception(file_path):
+def handle_exception(file_path, message, stacktrace):
+    if not os.path.exists(ERROR_LOG_PATH):
+        os.makedirs(ERROR_LOG_PATH)
     with open(ERROR_LOG_FILE, 'w+') as f:
-        print("hi")
-
+        f.write('{},{},{},{}\n'.format(str(datetime.datetime.now()), message, file_path, stacktrace))
 
 
 # MAIN #
-
 
 if __name__ == '__main__':
     process_set(TRAINING_SET_FILE_PATH)
