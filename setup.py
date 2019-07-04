99bfef7e076e0c59da17f0121cc399f4602ed237
buster-darragh-major <busterjdarraghmajor@gmail.com>
2019-07-04 17:34:05 +1200

    Update staticfg dependency to forked version

--- setup.py
+++ setup.py
@@ -1,16 +1,20 @@
 #!/usr/bin/env python3
 
 from setuptools import setup
+import os
 
 setup(name='cbt',
       description='Generating code using machine learning',
       author='Buster & Nathan',
       packages=['cbt'],
-      dependency_links=['https://github.com/codeauroraforum/comment-filter/tarball/master#egg=comment-filter'],
       install_requires=[
             'staticfg',
             'networkx',
             'graphviz',
             'pydot',
             'comment-filter'
+      ],
+      dependency_links=[
+            'https://github.com/codeauroraforum/comment-filter/tarball/master#egg=comment-filter-v1.0.0',
+            'https://github.com/Buster-Darragh-Major/staticfg/tarball/master#egg=staticfg-0.9.5'
       ])
