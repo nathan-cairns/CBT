05d0685eb3b707f925fcd286488cb67757972a01
Nathan Cairns <nathancairns777@gmail.com>
2019-07-28 15:40:36 +1200

    Create evaluation skeleton

---
+++ scripts/evaluate.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+
+# This script is used for Evaluating the results of a trained CBT model.
+# Author: Nathan Cairns
+
+
+# IMPORTS #
+
+
+# CONSTANTS #
+
+
+# FUNCTIONS #
+
+def style_fails():
+    pass
+
+
+def critical_fails():
+    pass
+
+
+def executable():
+    pass
+
+
+# MAIN #
+
+
+if __name__ == '__main__':
+    pass
