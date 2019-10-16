3b8c2bcaed831b306cec17dbda84f444a1cba525
buster-darragh-major <busterjdarraghmajor@gmail.com>
2019-10-16 17:12:03 +1300

    Remove unecessary code beautify import

--- setup.py
+++ setup.py
@@ -22,8 +22,3 @@ setup(name='cbt',
       dependency_links=[
             'https://github.com/codeauroraforum/comment-filter/tarball/master#egg=comment-filter-v1.0.0'
       ])
-
-print("Downloading additional libraries:")
-subprocess.call(['curl', '-O',
-                 '"https://github.com/ayonious/C-Code-Beautifier/releases/download/v0.7/C-Code-Beautifier"', 'chmod',
-                 '+x', os.path.join('lib', 'C-Code-Beautifier')])
